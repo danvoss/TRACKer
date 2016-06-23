d50dc988f2f296822bd1fd7fea187ce7fecfd5ec
Dan <danielandrewvoss@gmail.com>
2016-06-23 15:54:35 -0400

    Create home.html and login/logout routes

--- src/main/java/com/dvoss/controllers/TrackerController.java
+++ src/main/java/com/dvoss/controllers/TrackerController.java
@@ -1,11 +1,15 @@
 package com.dvoss.controllers;
 
+import com.dvoss.entities.User;
 import com.dvoss.services.MeetRepository;
 import com.dvoss.services.UserRepository;
+import com.dvoss.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Dan on 6/23/16.
@@ -20,6 +24,26 @@ public class TrackerController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home() {
         return "home";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(HttpSession session, String username, String password) throws Exception {
+        User user = users.findByName(username);
+        if (user == null) {
+            user = new User(username, PasswordStorage.createHash(password));
+            users.save(user);
+        }
+        else if (!PasswordStorage.verifyPassword(password, user.getPassword())) {
+            throw new Exception("Wrong password.");
+        }
+        session.setAttribute("username", username);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
     }
 
 
--- src/main/java/com/dvoss/services/UserRepository.java
+++ src/main/java/com/dvoss/services/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by Dan on 6/23/16.
  */
 public interface UserRepository extends CrudRepository<User, Integer> {
+    public User findByName(String username);
 }
