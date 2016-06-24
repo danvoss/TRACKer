5bb36daa4122d01367cbe53ce5a7ee98eba45738
Dan <danielandrewvoss@gmail.com>
2016-06-24 10:53:42 -0400

    Add create route

--- src/main/java/com/dvoss/controllers/TrackerController.java
+++ src/main/java/com/dvoss/controllers/TrackerController.java
@@ -1,15 +1,18 @@
 package com.dvoss.controllers;
 
+import com.dvoss.entities.Meet;
 import com.dvoss.entities.User;
 import com.dvoss.services.MeetRepository;
 import com.dvoss.services.UserRepository;
 import com.dvoss.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
 
 /**
  * Created by Dan on 6/23/16.
@@ -22,7 +25,12 @@ public class TrackerController {
     MeetRepository meets;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home() {
+    public String home(HttpSession session, Model model) {
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("username", username);
+
+        Iterable<Meet> m = meets.findAll();
+        model.addAttribute("meets", m);
         return "home";
     }
 
@@ -43,6 +51,15 @@ public class TrackerController {
     @RequestMapping(path = "/logout", method = RequestMethod.POST)
     public String logout(HttpSession session) {
         session.invalidate();
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/create", method = RequestMethod.POST)
+    public String create(HttpSession session, String date, String location, String division, String gender, String winner, String comments ) {
+        String username = (String) session.getAttribute("username");
+        User user = users.findByName(username);
+        Meet meet = new Meet(LocalDate.parse(date), location, division, gender, winner, comments, user);
+        meets.save(meet);
         return "redirect:/";
     }
 
