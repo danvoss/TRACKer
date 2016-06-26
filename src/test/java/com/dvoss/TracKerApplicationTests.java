9999ac9ac4e5e6c8c18ffa0fb09e258091f28259
Dan <danielandrewvoss@gmail.com>
2016-06-26 17:15:04 -0400

    Add delete and update tests

--- src/test/java/com/dvoss/TracKerApplicationTests.java
+++ src/test/java/com/dvoss/TracKerApplicationTests.java
@@ -15,6 +15,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.time.LocalDate;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TracKerApplication.class)
 @WebAppConfiguration
@@ -36,23 +38,66 @@ public class TracKerApplicationTests {
 		mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
 	}
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 	@Test
 	public void testLogin() throws Exception {
 		mockMvc.perform(
 				MockMvcRequestBuilders.post("/login")
-				.param("name", "TestUser")
+				.param("username", "TestUser")
 				.param("password", "pw")
 		);
 		Assert.assertTrue(users.count() == 1);
 	}
 
 	@Test
-	public void testCreate() {
+	public void testCreate() throws Exception {
+		testLogin();
 
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/create")
+				.param("date", LocalDate.now().toString())
+				.param("location", "TestLocation")
+				.param("division", "TestDivision")
+				.param("gender", "TestGender")
+				.param("winner", "TestWinner")
+				.param("comments", "TestComments")
+				.sessionAttr("username", "TestUser")
+		);
+		Assert.assertTrue(meets.count() == 1);
+	}
+
+	//  -- how do I get the id? :
+	@Test
+	public void testDelete() throws Exception {
+		testLogin();
+		testCreate();
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/delete")
+				.sessionAttr("username", "TestUser")
+				//.param("id", ?)
+		);
+		Assert.assertTrue(meets.count() == 0);
+	}
+
+	@Test
+	public void testUpdate() throws Exception {
+		testLogin();
+		testCreate();
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/update")
+				.param("date", LocalDate.now().toString())
+				.param("location", "NewLoc")
+				.param("division", "NewDiv")
+				.param("gender", "NewGen")
+				.param("winner", "NewWin")
+				.param("comments", "NewCom")
+				.sessionAttr("username", "TestUser")
+				//.param("id", ? )
+		);
+		Assert.assertTrue(meets.count() == 1);
 	}
 
 }
